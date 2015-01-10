b4a2ffadc3aac5923c9c99666ab6875bb6a1ff79
Kian Ho <hui.kian.ho@googlemail.com>
2015-01-10 20:38:59 +1100

    Added distance matrix test cases.

--- tests/test_pconpy.py
+++ tests/test_pconpy.py
@@ -9,135 +9,65 @@ Author:
     Kian Ho <hui.kian.ho@gmail.com>
 
 Description:
-    ...
+    This file contains the unit tests for pconpy.
 
 Usage:
-    test_pconpy.py
+    This script can be run by calling "nosetests" or "py.test" from the root
+    pconpy directory.
 
 """
 
 import os
 import sys
 
-#import pconpy
+import pconpy
 import numpy
-import unittest
-import Bio.PDB
-
-from itertools import ifilter, combinations
 
 PWD = os.path.dirname(os.path.realpath(__file__))
 PDB_DIR = os.path.join(PWD, "pdb_files")
 
 
-def load_pdb(pdb_fn, chain_id=None):
-    """Load a single chain from a pdb file.
-
-    """
-
-    return pconpy.pdb_to_ag(pdb_fn, chain_id)
-
-
-def get_residues(pdb_fn, chain_id=None, model_num=0):
-    """Build a simple list of residues from a single chain.
-
-    Arguments:
-        pdb_fn --
-        chain_id --
-        model_num --
-
-    Returns:
-        ...
-
-    """
-
-    pdb_id = os.path.splitext(os.path.basename(pdb_fn))[0]
-
-    parser = Bio.PDB.PDBParser(pdb_id, pdb_fn)
-    struct = parser.get_structure(pdb_id, pdb_fn)
-    model = struct[model_num]
-
-    if chain_id is None:
-        chain = model.get_list()[0]
-    else:
-        chain = model[chain_id]
-
-    residues = []
-
-    # To do, handle:
-    # - disordered residues/atoms (todo)
-    # - non-amino acids
-    for res in ifilter(lambda r : Bio.PDB.is_aa(r), chain.get_residues()):
-        if not Bio.PDB.is_aa(res):
-            sys.stderr.write("WARNING: non-standard AA at %r%s"
-                    % (res.get_id(), os.linesep))
-        residues.append(res)
-
-    return residues
-
-
-def calc_distance(res_a, res_b, method="ca"):
-    """Calculate the Euclidean distance between a pair of residues according to
-    a specified distance method.
-
-    """
-
-    if method in ("ca", "cb"):
-        # Conventional CA-CA or CB-CB distance.
-        atom_name = method.upper()
-
-        assert(atom_name in res_a)
-        assert(atom_name in res_b)
-
-        atom_a = res_a[atom_name] 
-        atom_b = res_b[atom_name]
-
-        A = atom_a.get_coord()
-        B = atom_b.get_coord()
-
-        dist = numpy.linalg.norm(A-B)
-    else:
-        raise NotImplementedError
-
-    return dist
-
-
-class TestGeometry(unittest.TestCase):
-    def setUp(self):
+class TestGeometry:
+    # Skeleton fixtures (will use in the future)
+    @classmethod
+    def setup_class(self):
         """
-
-        """
-
-        pdb_id = "1ejg"
-        pdb_fn = os.path.join(PDB_DIR, "{}.pdb".format(pdb_id))
-
-        self.residues = get_residues(pdb_fn, "A")
-
-        return
-
-
-    def tearDown(self):
-        """
-
         """
 
         return
 
-
-    def test_calc_distance(self):
+    @classmethod
+    def teardown_class(self):
         """
-
         """
-
-        #pconpy.calc_eucl_distance()
-        residues = self.residues
-
-        # upper triangle indices only
-        for i, j in combinations(range(len(residues)), 2):
-            res_i = residues[i]
-            res_j = residues[j]
-
-            print calc_distance(res_i, res_j, "ca")
-
 
         return
+
+    def pdb_id_to_fn(self, pdb_id):
+        """Get the path to a test pdb file using its pdb id.
+
+        """
+
+        return os.path.join(PDB_DIR, "{}.pdb".format(pdb_id))
+
+
+    def test_make_dist_matrix(self):
+        """Test the generation of standard Euclidean distance matrices.
+
+        """
+
+        # Ideal case should result in a square non-zero distance matrix.
+        pdb_fn = self.pdb_id_to_fn("1ubq")
+        residues = pconpy.get_residues(pdb_fn)
+        mat = pconpy.make_dist_matrix(residues)
+
+        assert( mat is not None )
+        assert( numpy.any(mat > 0) )
+        assert( mat.shape == (len(residues), len(residues)) )
+
+        # Empty residues list should result in an empty distance matrix.
+        residues = []
+        mat = pconpy.make_dist_matrix(residues)
+
+        assert( mat is not None )
+        assert( mat.shape == (0,0) )
