04505614c716b4f8aa359ab1c8774243c8693cb2
Kian Ho <hui.kian.ho@googlemail.com>
2015-03-06 18:19:10 +1100

    Added a basic setup.py file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="pconpy",
+    version="1.0",
+    author="Kian Ho",
+    author_email="h.kian.ho@gmail.com",
+    url="http://www.github.com/kianho/pconpy",
+    packages=["pconpy"]
+)
