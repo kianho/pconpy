a05f4011a448e1c3d1dde24f4af65639e8e6317e
Kian Ho <hui.kian.ho@googlemail.com>
2015-03-09 00:39:59 +1100

    Modified setup.py
    Removed the long list of dependencies since there is a known issue with
    including numpy in this list (see
    https://github.com/numpy/numpy/issues/2434)

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="pconpy",
-    version="1.0",
+    version="0.1",
+    description="a python module for generating protein contact maps and distance maps",
     author="Kian Ho",
-    author_email="h.kian.ho@gmail.com",
+    author_email="hui.kian.ho@gmail.com",
     url="http://www.github.com/kianho/pconpy",
-    packages=["pconpy"]
+    license="MIT",
+    packages=["pconpy"],
+    keywords=["protein", "protein structure", "bioinformatics", "contact map",
+        "distance map", "computational biology", "visualization",
+        "visualisation", "amino acids"],
+
+    # Dependencies
+    #install_requires=["biopython>=1.65", "numpy>=1.9.0", "matplotlib>=1.4.0", "docopt>=0.6.2"]
+    #install_requires=["biopython>=1.65", "matplotlib>=1.4.0", "docopt>=0.6.2"]
+    install_requires=["docopt>=0.6.2"]
 )
